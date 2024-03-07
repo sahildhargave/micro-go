d61ba489aad4c2aa0987ff667622d4eb360f7561
Sahildhargave <sahildhargave5253.com>
2024-03-06 23:11:35 -0800

    correcting user.sql.go

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -10,15 +10,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
 
-func createRandomAccount(t *testing.T) Account{
-	arg:= CreateAccountParams{
-	Owner: util.RandomOwner(),
-	Balance: util.RandomMoney(),
-	Currency: util.RandomCurrency(),
- }
+	arg := CreateAccountParams{
+		Owner:    user.Username,
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
 
- account, err := testQueries.CreateAccount(context.Background(), arg)
+	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
 
@@ -32,19 +33,16 @@ func createRandomAccount(t *testing.T) Account{
 	return account
 }
 func TestCreateAccount(t *testing.T) {
-  createRandomAccount(t)
+	createRandomAccount(t)
 }
 
-
-
-
-func TestGetAccount(t *testing.T){
+func TestGetAccount(t *testing.T) {
 	account1 := createRandomAccount(t)
 	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
-    require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.ID, account2.ID)
 	require.Equal(t, account1.Owner, account2.Owner)
 	require.Equal(t, account1.Balance, account2.Balance)
 	require.Equal(t, account1.Currency, account2.Currency)
@@ -52,11 +50,11 @@ func TestGetAccount(t *testing.T){
 
 }
 
-func TestUpdateAccount(t *testing.T){
+func TestUpdateAccount(t *testing.T) {
 	account1 := createRandomAccount(t)
 
 	arg := UpdateAccountParams{
-		ID : account1.ID ,
+		ID:      account1.ID,
 		Balance: util.RandomMoney(),
 	}
 
@@ -64,7 +62,6 @@ func TestUpdateAccount(t *testing.T){
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
-	
 	require.Equal(t, account1.ID, account2.ID)
 	require.Equal(t, account1.Owner, account2.Owner)
 	require.Equal(t, arg.Balance, account2.Balance)
@@ -72,7 +69,7 @@ func TestUpdateAccount(t *testing.T){
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
 
-func TestDeleteAccount(t *testing.T){
+func TestDeleteAccount(t *testing.T) {
 	account1 := createRandomAccount(t)
 
 	err := testQueries.DeleteAccount(context.Background(), account1.ID)
@@ -84,16 +81,16 @@ func TestDeleteAccount(t *testing.T){
 	require.Empty(t, account2)
 }
 
-func TestListAccounts(t *testing.T){
+func TestListAccounts(t *testing.T) {
 	var lastAccount Account
 
-	for i:= 0;i<10;i++{
+	for i := 0; i < 10; i++ {
 		lastAccount = createRandomAccount(t)
 	}
 
-	arg:= ListAccountsParams{
-		Owner: lastAccount.Owner,
-		Limit: 5,
+	arg := ListAccountsParams{
+		Owner:  lastAccount.Owner,
+		Limit:  5,
 		Offset: 0,
 	}
 
@@ -101,7 +98,7 @@ func TestListAccounts(t *testing.T){
 	require.NoError(t, err)
 	require.NotEmpty(t, accounts)
 
-	for _,account := range accounts{
+	for _, account := range accounts {
 		require.NotEmpty(t, account)
 		require.Equal(t, lastAccount.Owner, account.Owner)
 	}
--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -10,6 +10,45 @@ import (
 	_"github.com/jackc/pgx/v5/pgtype"
 )
 
+const createUser = `-- name: CreateUser :one
+INSERT INTO users (
+  username,
+  hashed_password,
+  full_name,
+  email
+) VALUES (
+  $1, $2, $3, $4
+) RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
+`
+
+type CreateUserParams struct {
+	Username       string `json:"username"`
+	HashedPassword string `json:"hashed_password"`
+	FullName       string `json:"full_name"`
+	Email          string `json:"email"`
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, createUser,
+		arg.Username,
+		arg.HashedPassword,
+		arg.FullName,
+		arg.Email,
+	)
+	var i User
+	err := row.Scan(
+		&i.Username,
+		&i.HashedPassword,
+		&i.FullName,
+		&i.Email,
+		&i.PasswordChangedAt,
+		&i.CreatedAt,
+		
+	)
+	return i, err
+}
+
+
 const getUser = `-- name: GetUser :one
 SELECT username, hashed_password, full_name, email, password_changed_at, created_at FROM users
 WHERE username = $1 LIMIT 1
